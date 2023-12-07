1a467f8bf7f4957cca644b9940e9d044e5997e89
Cal <calum.moore@gmail.com>
2023-12-07 20:02:38 +0000

    Fib test

--- src/lib.rs
+++ src/lib.rs
@@ -1,130 +1,223 @@
-use halo2_base::halo2_proofs::{
-    self,
-    circuit::SimpleFloorPlanner,
-    plonk::{self, Advice, Circuit, Column, ConstraintSystem, Expression, Instance, Selector},
-    poly::Rotation,
-};
+#![allow(clippy::type_complexity)]
+use halo2_base::halo2_proofs::{circuit::*, halo2curves::FieldExt, plonk::*, poly::Rotation};
 use std::marker::PhantomData;
 
-#[derive(Clone, Default)]
-pub struct IsTenCircuit<F> {
+#[derive(Debug, Clone)]
+pub struct FibConfig {
+    pub advice: [Column<Advice>; 3],
+    pub selector: Selector,
+    pub instance: Column<Instance>,
+}
+
+pub struct FibChip<F: FieldExt> {
+    config: FibConfig,
     _marker: PhantomData<F>,
 }
 
-#[derive(Clone)]
-pub struct IsTenCircuitConfig {
-    selector: Selector,
-    advice: Column<Advice>,
-    instance: Column<Instance>,
-}
-
-impl<F: halo2_base::halo2_proofs::arithmetic::FieldExt> Circuit<F> for IsTenCircuit<F> {
-    type Config = IsTenCircuitConfig;
-    type FloorPlanner = SimpleFloorPlanner;
-
-    fn without_witnesses(&self) -> Self {
-        Self::default()
+impl<F: FieldExt> FibChip<F> {
+    pub fn construct(config: FibConfig) -> FibChip<F> {
+        Self {
+            config,
+            _marker: PhantomData,
+        }
     }
 
-    fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
-        let selector = meta.selector();
-        let advice = meta.advice_column();
-        let instance = meta.instance_column();
+    // Define a custom gate here
+    pub fn configure(
+        advice: [Column<Advice>; 3],
+        instance: Column<Instance>,
+        cs: &mut ConstraintSystem<F>,
+    ) -> FibConfig {
+        let col_a = advice[0];
+        let col_b = advice[1];
+        let col_c = advice[2];
+        let selector = cs.selector();
 
-        meta.enable_equality(instance);
-        meta.enable_equality(advice);
+        // Every column that will be checked against a value in another column
+        // needs to be enabled for equality
+        cs.enable_equality(col_a);
+        cs.enable_equality(col_b);
+        cs.enable_equality(col_c);
+        cs.enable_equality(instance);
 
-        meta.create_gate("=10", |meta| {
-            let s = meta.query_selector(selector);
-            let a = meta.query_advice(advice, Rotation::cur());
-            vec![s * (a - Expression::Constant(F::from_u128(10u128)))]
+        cs.create_gate("add", |cells| {
+            let s = cells.query_selector(selector);
+
+            // Rotation::cur() is just a helper for 1 (any isize offset is allowed)
+            // Boring rotations are better for performance
+            let a = cells.query_advice(col_a, Rotation::cur());
+            let b = cells.query_advice(col_b, Rotation::cur());
+            let c = cells.query_advice(col_c, Rotation::cur());
+
+            // If s = 0 (not turned on selector) - a, b, c can be anything and gate will still be 0
+            // If s = 1 (turned on selector), the constraint needs to equal 0
+            vec![s * (a + b - c)]
         });
 
-        IsTenCircuitConfig {
+        FibConfig {
+            advice: [col_a, col_b, col_c],
             selector,
-            advice,
             instance,
         }
     }
 
-    fn synthesize(
+    pub fn assign_first_row(
         &self,
-        config: Self::Config,
-        mut layouter: impl halo2_proofs::circuit::Layouter<F>,
-    ) -> Result<(), plonk::Error> {
-        // Check public input is 10
+        mut layouter: impl Layouter<F>,
+    ) -> Result<(AssignedCell<F, F>, AssignedCell<F, F>, AssignedCell<F, F>), Error> {
         layouter.assign_region(
-            || "is_10?!",
+            || "first row",
             |mut region| {
-                config.selector.enable(&mut region, 0)?;
+                // Even the first row needs to match formula of gate
+                self.config.selector.enable(&mut region, 0)?;
 
-                // region.enable_selector(config.selector)?;
-                region.assign_advice_from_instance(
-                    || "10?",
-                    config.instance,
+                // Copies values from advice provider, we can only work with values in the advice
+                let a_cell = region.assign_advice_from_instance(
+                    || "a",
+                    self.config.instance,
+                    // Advice is a static column, so we can use absolute row offsets (basically just the vec![] we pass to prover)
                     0,
-                    config.advice,
+                    self.config.advice[0],
                     0,
                 )?;
 
-                Ok(())
+                let b_cell = region.assign_advice_from_instance(
+                    || "b",
+                    self.config.instance,
+                    // Advice is a static coumn, so we can use absolute row offsets
+                    1,
+                    self.config.advice[1],
+                    0,
+                )?;
+
+                println!("a: {:?}", a_cell.value());
+                println!("b: {:?}", b_cell.value());
+
+                let c_cell = region.assign_advice(
+                    || "c",
+                    self.config.advice[2],
+                    0,
+                    || {
+                        a_cell
+                            .value()
+                            .and_then(|a| b_cell.value().and_then(|b| Value::known(*a + *b)))
+                    },
+                )?;
+
+                Ok((a_cell, b_cell, c_cell))
             },
         )
+    }
+
+    pub fn assign_row(
+        &self,
+        mut layouter: impl Layouter<F>,
+        a: &AssignedCell<F, F>,
+        b: &AssignedCell<F, F>,
+    ) -> Result<AssignedCell<F, F>, Error> {
+        layouter.assign_region(
+            || "next_row",
+            |mut region| {
+                self.config.selector.enable(&mut region, 0)?;
+
+                // Copies the value from an assigned cell to another cell
+                // THIS IS A CONSTRAINT TOO - this ensures that each row follows the other!
+                a.copy_advice(|| "a", &mut region, self.config.advice[0], 0)?;
+                b.copy_advice(|| "b", &mut region, self.config.advice[1], 0)?;
+
+                let c_value = a.value().and_then(|a| b.value().map(|b| *a + *b));
+
+                let c = region.assign_advice(|| "c", self.config.advice[2], 0, || c_value)?;
+
+                // We return C from the region, this is how we can access region values outside of a region!
+                Ok(c)
+            },
+        )
+    }
+
+    // Instance is global
+    pub fn expose_public(
+        &self,
+        mut layouter: impl Layouter<F>,
+        cell: &AssignedCell<F, F>,
+        row: usize,
+    ) -> Result<(), Error> {
+        layouter.constrain_instance(cell.cell(), self.config.instance, row)
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use halo2_base::halo2_proofs::{
-        dev::metadata::{Column, VirtualCell},
-        dev::{FailureLocation, MockProver, VerifyFailure},
-        halo2curves::bn256::Fr,
-        halo2curves::FieldExt,
-    };
+    use halo2_base::halo2_proofs::dev::MockProver;
+    use halo2_base::halo2_proofs::halo2curves::bn256::Fr;
 
-    #[test]
-    fn test_valid_circuit() {
-        let k = 21;
-        let circuit = IsTenCircuit::<Fr>::default();
-        let public_inputs = vec![Fr::from_u128(10u128)];
-        let prover = MockProver::run(k, &circuit, vec![public_inputs]).unwrap();
+    #[derive(Default)]
+    struct MyCircuit {
+        fib_size: usize,
+    }
 
-        // Check we don't panic
-        prover.assert_satisfied();
+    impl<F: FieldExt> Circuit<F> for MyCircuit {
+        type Config = FibConfig;
+        type FloorPlanner = SimpleFloorPlanner;
+
+        fn without_witnesses(&self) -> Self {
+            Self::default()
+        }
+
+        // Circuit setup (doesn't change on input)
+        fn configure(cs: &mut ConstraintSystem<F>) -> Self::Config {
+            let col_a = cs.advice_column();
+            let col_b = cs.advice_column();
+            let col_c = cs.advice_column();
+            let instance = cs.instance_column();
+
+            FibChip::configure([col_a, col_b, col_c], instance, cs)
+        }
+
+        // Changes for each proof
+        fn synthesize(
+            &self,
+            config: Self::Config,
+            mut layouter: impl Layouter<F>,
+        ) -> Result<(), Error> {
+            let chip = FibChip::construct(config);
+
+            let (_, mut b, mut c) = chip.assign_first_row(layouter.namespace(|| "first row"))?;
+
+            // We've skipped the first 2 items in fib sequence (as they are awkward)
+            // 0 should be 3, just using 0 to test row count - 57 should be =9
+            for _ in 3..=self.fib_size - 1 {
+                let new_c = chip.assign_row(layouter.namespace(|| "next_row"), &b, &c)?;
+                b = c;
+                c = new_c;
+            }
+
+            println!("c: {:?}", c.value());
+
+            // chip.expose_public(layouter.namespace(|| "out"), &c, 2)?;
+
+            Ok(())
+        }
     }
 
     #[test]
-    fn test_invalid_circuit() {
-        let k = 21;
-        let circuit = IsTenCircuit::<Fr>::default();
+    fn main() {
+        let k = 20;
 
-        // Check the circuit errors for all values except 10
-        for i in 0..20u128 {
-            // 10 is valid, so skip it
-            if i != 10 {
-                let public_inputs = vec![Fr::from_u128(i)];
-                let prover = MockProver::run(k, &circuit, vec![public_inputs]).unwrap();
+        let a = Fr::from(1);
+        let b = Fr::from(1);
+        // let out = Fr::from(102334155);
 
-                assert_eq!(
-                    prover.verify(),
-                    Err(vec![VerifyFailure::ConstraintNotSatisfied {
-                        constraint: ((0, "=10").into(), 0, "").into(),
-                        location: FailureLocation::InRegion {
-                            region: (0, "is_10?!").into(),
-                            offset: 0,
-                        },
-                        cell_values: vec![(
-                            VirtualCell::from((Column::from((Advice::default().into(), 0)), 0)),
-                            if i <= 1 {
-                                format!("{i:x}")
-                            } else {
-                                format!("0x{i:x}")
-                            }
-                        )]
-                    }])
-                )
-            }
-        }
+        let circuit = MyCircuit { fib_size: 1000000 };
+
+        // Vector for the public input column (if we had more, we'd need to add additional)
+        let public_input = vec![a, b];
+        let instance_columns = vec![public_input];
+
+        let prover = MockProver::<Fr>::run(k, &circuit, instance_columns).unwrap();
+        prover.assert_satisfied();
+
+        println!("Proof generated successfully!");
     }
 }
